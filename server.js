7743dbd1bf7d6dbd5ba2370b8318a426a025fab4
Obire John <youremail@gmail.com>
2026-03-06 11:35:35 -0800

    fix route filename

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-console.log(process.env.MONGO_URI);
 
 const connectDB = require("./config/db");
-const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
@@ -12,6 +10,7 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/user", require("./routes/userRoutes"));
 app.use("/api/transactions", require("./routes/transactionRoutes"));
@@ -20,7 +19,7 @@ app.get("/", (req, res) => {
   res.send("SecureBank API Running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
